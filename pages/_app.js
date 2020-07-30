d5e9638d030d8312fa45670b9ec2e8e9db01f586
Naufal Alauddin Hilmi <naufal.alauddin@ui.ac.id>
2020-07-30 12:06:32 +0700

    [CHORES] configure nextjs pages to import muli

---
+++ pages/_app.js
@@ -0,0 +1,41 @@
+import App from 'next/app';
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <div>
+        <Component {...pageProps} />
+        <style jsx global>{`
+          @font-face {
+            font-family: 'Muli';
+            font-style: normal;
+            font-weight: 400;
+            src: url('/fonts/muli/Muli.ttf') format('truetype');
+          }
+          @font-face {
+            font-family: 'Muli';
+            font-style: normal;
+            font-weight: 600;
+            src: url('/fonts/muli/Muli-SemiBold.ttf') format('truetype');
+          }
+          @font-face {
+            font-family: 'Muli';
+            font-style: normal;
+            font-weight: 800;
+            src: url('/fonts/muli/Muli-Bold.ttf') format('truetype');
+          }
+          @font-face {
+            font-family: 'Muli';
+            font-style: normal;
+            font-weight: 200;
+            src: url('/fonts/muli/Muli-Light.ttf') format('truetype');
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
+
+export default MyApp;

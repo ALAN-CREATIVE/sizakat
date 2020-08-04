69aab145412304c0debe56cfdb602839f6b7c445
Naufal Alauddin Hilmi <naufal.alauddin@ui.ac.id>
2020-08-04 13:52:57 +0700

    [REFACTOR] fix Filter component

--- components/Filters/Filter.js
+++ components/Filters/Filter.js
@@ -1,36 +1,35 @@
 import React, { useState } from 'react';
 import { FilterStyle } from './FilterStyle';
 
+const Filter = ({label, options, error, onRadioClicked}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleOnChange = (event) => {
+    onRadioClicked(event.target.value);
+  }
 
-
-const Filter = ({options,required,error,onRadioClicked}) => {
-    // ini namanya statement
-    const [isOpen, setIsOpen] = useState(false);
-    const handleOnChange = (event) => {
-        onRadioClicked(event.target.value);
-    }
-    const [isSelect, setSelect] = useState(null);
-
-    return (
-        <div className='filter'>
-            <FilterStyle />
-            <div onClick={() => setIsOpen(isOpen ? false : true)}>
-            <p>SEMUA SUMBER DATA ⯆ </p>
-                {isOpen &&
-                    <div className='kotakFilter'>
-                        <div className="radios" onChange={handleOnChange} >{options.map(option => (
-                            <div className="radio" key={option} >
-                                <input type="radio" value={option} key={`input-${option}`} />
-                                <label >{ option }</label>
-                            </div>
-                        ))}</div>
-                        { error && <span className="error">{ error }</span> }
-                    </div>
-                }
-            </div>
-            
+  return (
+    <div className='filter'>
+      <FilterStyle />
+      <div onClick={() => setIsOpen(isOpen ? false : true)}>
+        <div>
+          { label }
+          <img style={{verticalAlign: 'baseline'}} src="/dropdown-arrow.svg" />
         </div>
-    );
+        <div className={`kotakFilter ${isOpen ? '': 'hide'}`}>
+          <div className="radios" onChange={handleOnChange}>
+            {options.map(option => (
+              <div className="radio" key={option} >
+                <label style={{display: 'block'}}>
+                  <input name="filter" type="radio" value={option} key={`input-${option}`} />
+                  { option }
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Filter;
--- components/Filters/FilterStyle.js
+++ components/Filters/FilterStyle.js
@@ -1,26 +1,42 @@
 import React from 'react';
 
 export const FilterStyle = () => (
-    <style jsx>{`
-      .filter p{
-        font-family: Muli;
-        font-style: normal;
-        font-weight: bold;
-        font-size: 14px;
-        line-height: 18px;
-        color: #00239D;
-        text-decoration : none;
-        }
-
-      .kotakFilter{
-        position : absolute;
-        width : 55%;
-        height : 55%;
-        padding : 3%;
-        background:  #E5E5E5;
-        border: 1px solid #393F50;
-        box-sizing: border-box;
+  <style jsx>{`
+    .filter {
+      font-family: Muli;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 14px;
+      line-height: 18px;
+      color: #00239D;
+      text-decoration : none;
       }
-      
-    `}</style>
+
+    .kotakFilter {
+      font-weight: 600;
+      font-size: 16px;
+      color: #393F50;
+      position : absolute;
+      top: 50%;
+      left: 50%;
+      width: 500px;
+      padding : 40px 10px;
+      width: 500px;
+      transform: translate(-50%, -50%);
+      border: 1px solid #393F50;
+      box-sizing: border-box;
+    }
+
+    .radio {
+      margin: 20px 10px;
+    }
+
+    .radio input {
+      margin-right: 30px;
+    }
+
+    .hide {
+      display: none;
+    }
+  `}</style>
 );

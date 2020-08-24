a10f81cc4ab10403cf1c475f562119fb777539b9
Zafira Binta <bintazafira@gmail.com>
2020-08-24 10:10:43 +0700

    update storyshot pake yarn test -u

--- components/UbahMustahik/UbahMustahik.js
+++ components/UbahMustahik/UbahMustahik.js
@@ -339,7 +339,6 @@ export default function update() {
   return (
     <>
       <head>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossOrigin="anonymous"></link>
         <link href="https://fonts.googleapis.com/css?family=Muli:300,400,700,800" rel="stylesheet" />
       </head>
       <main>        
